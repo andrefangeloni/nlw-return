38e0ccf095106073852c4dbb485b01059f3d374b
André Angeloni <andrefangeloni@gmail.com>
2022-05-31 19:29:30 -0300

    feat: feedback type step into component

---
+++ frontend/src/components/Widget/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,13 @@
+import { CloseButton } from '../../Button/Close';
+
+export const FeedbackContentStep = () => (
+  <>
+    <header>
+      <span className="text-xl leading-6">Deixe seu feedback</span>
+
+      <CloseButton />
+    </header>
+
+    <div className="flex py-8 gap-2 w-full"></div>
+  </>
+);
