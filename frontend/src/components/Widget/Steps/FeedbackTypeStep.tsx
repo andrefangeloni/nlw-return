b6e6086fbbf98323cacaa6eec774329d32ec950f
André Angeloni <andrefangeloni@gmail.com>
2022-07-20 17:22:54 -0300

    fix: image button

--- frontend/src/components/Widget/Steps/FeedbackTypeStep.tsx
+++ frontend/src/components/Widget/Steps/FeedbackTypeStep.tsx
@@ -20,7 +20,7 @@ export const FeedbackTypeStep = ({
         <button
           key={key}
           type="button"
-          className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+          className="bg-zinc-800 rounded-lg py-5 w-24 flex flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
           onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
         >
           <img src={value.image.source} alt={value.image.alt} />
